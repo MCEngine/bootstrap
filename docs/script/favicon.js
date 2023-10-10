de4641010f238f5e7378986a4d28689064219328
owen <56456776+JetsadaWijit@users.noreply.github.com>
2023-10-10 07:34:14 +0000

    update

---
+++ docs/script/favicon.js
@@ -0,0 +1,10 @@
+// Create a link element for the favicon
+const faviconContent = document.createElement('link');
+  
+// Set the attributes for the favicon link element
+faviconContent.rel = 'icon';
+faviconContent.type = 'image/x-icon';
+faviconContent.href = 'https://i.imgur.com/X0Vl4PK.jpg';
+
+// Add the favicon link element to the document's head
+document.head.appendChild(faviconContent);
