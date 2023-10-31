2c63855c446eff5287c6b9c3c3afb2b89454723f
owen <56456776+JetsadaWijit@users.noreply.github.com>
2023-10-31 11:22:17 +0000

    update

--- docs/script/favicon.js
+++ docs/script/favicon.js
@@ -1,10 +1,25 @@
+async function getJsonData(key) {
+    try {
+        const response = await fetch('https://mcengine.github.io/cdn/data.json');
+        const data = await response.json();
+        const imgLogo = data[key];
+        return imgLogo;
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+        throw error; // Optional: Re-throw the error for further handling
+    }
+}
+
 // Create a link element for the favicon
 const faviconContent = document.createElement('link');
   
 // Set the attributes for the favicon link element
 faviconContent.rel = 'icon';
 faviconContent.type = 'image/x-icon';
-faviconContent.href = 'https://i.imgur.com/X0Vl4PK.jpg';
+(async () => {
+    const logo = await getJsonData('logo');
+    faviconContent.href = logo;
+})();
 
 // Add the favicon link element to the document's head
 document.head.appendChild(faviconContent);
